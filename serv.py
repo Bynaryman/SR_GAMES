739aeeb2b4cdf4165198764a9db1662a1cac14df
Bynaryman <i.f.lledoux@gmail.com>
2017-12-28 16:30:11 +0100

    improving PEP8 conventions

--- serv.py
+++ serv.py
@@ -1,6 +1,7 @@
 import rpyc
 from rpyc.utils.server import ThreadedServer # or ForkingServer
 
+
 class MyService(rpyc.Service):
     
     _players = []
@@ -14,11 +15,12 @@ class MyService(rpyc.Service):
         self._players.remove(self._conn)
         print('player left:', self._conn)
 
+
 if __name__ == "__main__":
 
     """
         note: we can chose ThreadedServer, ForkingServer or even ThreadedPoolServer
     """
 
-    server = ThreadedServer(MyService, port = 12345)
+    server = ThreadedServer(MyService, port=12345)
     server.start()
