c94c9a3aec653a9b91f77ad335b818ed6dddb780
Bynaryman <i.f.lldoux@gmail.com>
2017-12-28 15:52:18 +0100

    init commit

---
+++ client.py
@@ -0,0 +1,3 @@
+import rpyc
+
+conn = rpyc.connect("127.0.0.1", 12345)
---
+++ serv.py
@@ -0,0 +1,24 @@
+import rpyc
+from rpyc.utils.server import ThreadedServer # or ForkingServer
+
+class MyService(rpyc.Service):
+    
+    _players = []
+    _grid = []
+
+    def on_connect(self):
+        self._players.append(self._conn)
+        print('new player:', self._conn)
+
+    def on_disconnect(self):
+        self._players.remove(self._conn)
+        print('player left:', self._conn)
+
+if __name__ == "__main__":
+
+    """
+        note: we can chose ThreadedServer, ForkingServer or even ThreadedPoolServer
+    """
+
+    server = ThreadedServer(MyService, port = 12345)
+    server.start()
