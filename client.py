4b9f46b848d0d6e48736c871a6230df50aa959ec
JD <julien.durand.35@hotmail.fr>
2017-12-29 13:58:16 +0100

    client.py add display part

--- client.py
+++ client.py
@@ -1,10 +1,43 @@
 import rpyc
 import os
+import tkinter as tk
 
-
+class Display():
+	
+	def __init__(self,rows,columns,sizeOfCase):	
+		self._rows = rows
+		self._columns = columns
+		self._sizeOfCase = sizeOfCase
+		self._damier = tk.Canvas(width=self._rows*self._sizeOfCase, height=self._columns*self._sizeOfCase)
+		for k in range(0,self._rows):
+			self._damier.create_line(self._sizeOfCase*k, 0,self._sizeOfCase*k,self._rows*self._sizeOfCase, width=1)
+			self._damier.create_line(0, self._sizeOfCase*k,self._columns*self._sizeOfCase ,self._sizeOfCase*k, width=1)
+		self._damier.pack()
+		
+		
+	def draw(self,grid):
+		self._grid = grid
+		for x in range (0,len(self._grid)):
+			for y in range (0,len(self._grid[x])):
+				if (self._grid[x][y] == 1 ):
+					self._damier.create_rectangle(x * self._sizeOfCase, y * self._sizeOfCase, (x + 1) * self._sizeOfCase, (y + 1) * self._sizeOfCase, fill='red')
+				if (self._grid[x][y] == 2 ):
+					self._damier.create_rectangle(x * self._sizeOfCase, y * self._sizeOfCase, (x + 1) * self._sizeOfCase, (y + 1) * self._sizeOfCase, fill='green')
+							
+				
+			
+		
 if __name__ == '__main__':
-
-    conn = rpyc.connect('127.0.0.1', 12345)
-    conn2 = rpyc.connect('127.0.0.1', 12345)
-    print(conn.root.exposed_get_players())
-    os.system('pause')
+    #conn = rpyc.connect('127.0.0.1', 12345)
+    #conn2 = rpyc.connect('127.0.0.1', 12345)
+    #print(conn.root.exposed_get_players())
+    #os.system('pause')	
+	rows = columns = 10
+	sizeOfCase = 30	
+	tab1 = [0,1,0,0,1,2,0,2,0,0]
+	tab2 = [0,0,0,2,0,1,2,0,0,1]
+	tab = [tab1,tab2,tab1,tab2,tab1,tab2,tab1,tab2,tab1,tab2]
+	disp = Display(rows,columns,sizeOfCase)
+	disp.draw(tab)
+	tk.mainloop()
+	
