49688ffc1eb9742a3a3b0bc72cdbc789369c57dd
Bynaryman <i.f.lledoux@gmail.com>
2018-01-02 13:33:42 +0100

    serv.py no more init has it is suggested on rpyc doc

--- serv.py
+++ serv.py
@@ -14,25 +14,17 @@ class GameServer(rpyc.Service):
                                               [0, 0, 0, 0],
                                               [0, 0, 1, 0],
                                               [0, 0, 0, 0]]
-        _sweet_probability
-        _players
+         _players
 
     """
 
-    _world = []
+    _world = [[choices([0, 2], [0.7, 0.3])[0] for _ in range(10)] for _ in range(10)]
     _players = []
-    _sweet_probability = 0.3
-    _dim = (10, 10)
-
-    def __init__(self, dim):
-        self._conn = None
-        self._dim = dim
-        super().__init__(self)
-        self._init_world()
 
     """
         TODO
     """
+
     def __repr__(self):
         print('players: ', self._players, '\n')
         print('actual representation of world:\n')
@@ -52,11 +44,6 @@ class GameServer(rpyc.Service):
     def exposed_get_players(self):
         return self._players
 
-    def _init_world(self):
-        x, y = self._dim
-        self._world = [[choices([0, 2], [1 - self._sweet_probability, self._sweet_probability])[0] for _ in range(x)]
-                       for _ in range(y)]
-
 
 if __name__ == '__main__':
     """
@@ -64,8 +51,5 @@ if __name__ == '__main__':
         depending on the OS. Forking only works with UNIX based OS
     """
 
-    world_dim = (10, 10)
-    game_server = GameServer(world_dim)
-    server = ThreadedServer(game_server, port=12345)
-    print(game_server)
+    server = ThreadedServer(GameServer, port=12345)
     server.start()
