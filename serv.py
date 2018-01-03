1f1d817c4b31cac493f5c296d104cc50320d4677
Bynaryman <i.f.lledoux@gmail.com>
2018-01-03 10:36:46 +0100

    serv.py spawn of clients in random possible positions, change the struct of _players

--- serv.py
+++ serv.py
@@ -1,4 +1,4 @@
-from random import choices
+from random import choices, randint
 import rpyc
 from rpyc.utils.server import ThreadedServer  # or ForkingServer
 from time import time
@@ -15,7 +15,12 @@ class GameServer(rpyc.Service):
                                               [0, 0, 0, 0],
                                               [0, 0, 1, 0],
                                               [0, 0, 0, 0]]
-         _players
+
+        _players   A one dimension array of dictionaries
+
+                Example of the array at one moment :
+                [{player: conn1, coords:(1, 2)},
+                 {player: conn2, coords:(3, 2)}]
 
     """
 
@@ -25,6 +30,7 @@ class GameServer(rpyc.Service):
     """
         TODO
     """
+
     def __repr__(self):
         print('players: ', self._players, '\n')
         print('actual representation of world:\n')
@@ -34,14 +40,21 @@ class GameServer(rpyc.Service):
         return tmp
 
     def on_connect(self):
-        self._players.append(self._conn)
+        x, y = self.find_correct_place_to_spawn()
+        self._world[y][x] = 1
+        self._players.append({"player": self._conn, "coords": (x, y)})
         player_name = 'Player' + str(len(self._players) - 1)
         print('new player joined the game: ' + player_name)
-        for player in self._players:
+        for player in [d['player'] for d in self._players]:
             player.root.notify_new_player(player_name)
-            
+
+    """
+        called when a player is connected and want to spawn,
+        so the server finds him a correct place and say him to redraw
+    """
+
     def exposed_start_game(self):
-        self._conn.root.draw([[choices([0, 2], [0.7, 0.3])[0] for _ in range(10)] for _ in range(10)])
+        self._conn.root.draw(self._world)
         print("test")
 
     def on_disconnect(self):
@@ -50,13 +63,20 @@ class GameServer(rpyc.Service):
             del self._players[index]  # O(size - index)
             player_name = 'Player' + str(index)
             print('player left:', player_name)
-            for player in self._players:
+            for player in [d['player'] for d in self._players]:
                 player.root.notify_player_left(player_name)
         except ValueError:
             print('no such player')
 
     def exposed_get_players(self):
         return self._players
+
+    def find_correct_place_to_spawn(self):
+        dimX, dimY = len(self._world[0]), len(self._world)
+        randX, randY = randint(0, dimX - 1), randint(0, dimY - 1)
+        while self._world[randY][randX] != 0:
+            randX, randY = randint(0, dimX - 1), randint(0, dimY - 1)
+        return randX, randY
 
 
 if __name__ == '__main__':
