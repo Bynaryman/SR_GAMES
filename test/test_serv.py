286f76e43d9e21347f097c368bcb17df6c360774
bynaryman <i.f.lledoux@gmail.com>
2018-01-07 20:10:50 +0100

    test_server.py tentative mock

--- test/test_serv.py
+++ test/test_serv.py
@@ -1,24 +1,47 @@
-from mock import MagicMock
+import unittest
+
+from unittest.mock import MagicMock
 from src.serv import GameServer
 from src.client import GameClient
 from rpyc.utils.server import ThreadedServer
 import threading
 import rpyc
 
+
+class TestGameServer(unittest.TestCase):
+
+    def test_notify_new_player(self):
+
+        mock_server = GameServer(None)
+        # mock_server.
+        t = ThreadedServer(mock_server.__class__, port=8686)
+        serv_thread = threading.Thread(target=t.start)
+        serv_thread.start()
+
+        conn = rpyc.connect('127.0.0.1', 8686, service=GameClient)
+        conn2 = rpyc.connect('127.0.0.1', 8686, service=GameClient)
+
+        # conn.root.exposed_notify_new_player = MagicMock()
+
+        mock_client = GameClient(conn)
+        mock_client.exposed_notify_new_player = MagicMock()
+
+        conn.root.start_game('t')
+        conn.root.start()
+        print('here')
+
+        mock_client.exposed_notify_new_player.assert_called()
+        print('after')
+        self.assertTrue(True)
+
+
 if __name__ == '__main__':
 
     """
         In this module we mock client calls to assert server servcice behaviours
     """
+    unittest.main()
 
-    mock_client = GameClient(None)
-    mock_client.exposed_notify_new_player = MagicMock(return_value='valeur bid0n')
 
-    t = ThreadedServer(GameServer, port=8686)
 
-    print('here before')
-    serv_thread = threading.Thread(target=t.start)
-    serv_thread.start()
-    print('here')
-    conn = rpyc.connect('127.0.0.1', 8686, service=GameClient)
-    print(conn.root.exposed_start_game('toto'))
+
